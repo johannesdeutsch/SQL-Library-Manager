37d7c8864d6f2f50c3541ac217633f49b1dccbff
Johannes Deutsch <johannes.deutsch@gmx.at>
2023-04-04 22:02:29 +0200

    worked on index.pug, update-book.pug, index.js

--- routes/index.js
+++ routes/index.js
@@ -18,39 +18,39 @@ function asyncHandler(callback) {
 
 /* GET home page. */
 router.get('/', asyncHandler (async ( req, res, next ) => {
-  res.redirect('/books');
+  res.redirect('/books/');
 }));   
 
 //shows the full list of books
 router.get('/books', asyncHandler (async (req, res) => {
   const booklist = await Book.findAll();
-  res.render('/index', { booklist, pagetitle: "Books", pageheader: "Books"});
+  res.render('index', { booklist, pagetitle: "Books", pageheader: "Books"});
   console.log(booklist);
 }));
 
 // shows the create new book form
 router.get('/books/new', (req, res) => {
-  res.render('/new-book', { pagetitle: "New Book", pageheader: "New Book" });
+  res.render('new-book', { pagetitle: "New Book", pageheader: "New Book" });
 }); 
 
 // posts a new book to the data base
 router.post('/books/new', asyncHandler (async (req, res) => {
   const newBook = await Book.create(req.body);
-  res.redirect('/books' + newBook.id);
+  res.redirect('/books/' + newBook.id);
 }));
 
 //shows book detail form
 router.get('/books/:id', asyncHandler (async (req, res) => {
   const book = await Book.findByPk(req.params.id);
   console.log(book);
-  res.render('/update-book', { book, pageheader: "Update Book", pagetitle: book.title})
+  res.render('update-book', { book, pageheader: "Update Book", pagetitle: book.title})
 }));
 
 //updates books info in the database
 router.post('/books/:id', asyncHandler (async (req, res) => {
   const book = await Book.findByPk(req.params.id);
   await book.update(req.body);
-  res.redirect('/books');
+  res.redirect('/books/' + book.id);
 }));
 
 //delets a book
